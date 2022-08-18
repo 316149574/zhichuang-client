a9af496e6260ef17b7e295839be0283dc933c4dc
duanli <316149574@qq.com>
2022-08-18 14:26:07 +0800

    fix cdn

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 // import commonjs from "rollup-plugin-commonjs";
-import externalGlobals from "rollup-plugin-external-globals";
+// import externalGlobals from "rollup-plugin-external-globals";
 const path = require('path');
 
 // https://vitejs.dev/config/
@@ -10,15 +10,15 @@ export default defineConfig({
     port: 8088
   },
   build:{
-    rollupOptions:{
-      external:["vue","element-plus"],
-      plugins:[
-        externalGlobals({
-          vue: "Vue",
-          "element-plus": "ElementPlus"
-        }),
-      ]
-    }
+    // rollupOptions:{
+    //   external:["vue","element-plus"],
+    //   plugins:[
+    //     externalGlobals({
+    //       vue: "Vue",
+    //       "element-plus": "ElementPlus"
+    //     }),
+    //   ]
+    // }
   },
   plugins: [ 
   vue()],
