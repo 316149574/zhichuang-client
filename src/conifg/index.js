41fdd182aca4ad27af18a949e5eb97a1c055a567
duanli_666 <316149574@qq.com>
2023-03-02 09:16:24 +0800

    change url

--- src/conifg/index.js
+++ src/conifg/index.js
@@ -1,21 +1,21 @@
-const env = import.meta.env.MODE || 'prod';
-const EnvConfig ={
-    dev:{
-        baseApi:'http://zhichuang.team:3000',
-        mockApi:'https://www.fastmock.site/mock/ea387bf6b0a1f5a493bc2af328a2d028'
-    },
-    test:{
-        baseApi:'http://zhichuang.team:3000',
-        mockApi:'https://www.fastmock.site/mock/ea387bf6b0a1f5a493bc2af328a2d028'
-    },
-    prod:{
-        baseApi:'http://zhichuang.team:3000',
-        mockApi:'https://www.fastmock.site/mock/ea387bf6b0a1f5a493bc2af328a2d028'
-    }
-}
+const env = import.meta.env.MODE || "prod";
+const EnvConfig = {
+  dev: {
+    baseApi: "119.3.202.71:3000",
+    mockApi: "https://www.fastmock.site/mock/ea387bf6b0a1f5a493bc2af328a2d028",
+  },
+  test: {
+    baseApi: "119.3.202.71:3000",
+    mockApi: "https://www.fastmock.site/mock/ea387bf6b0a1f5a493bc2af328a2d028",
+  },
+  prod: {
+    baseApi: "119.3.202.71:3000",
+    mockApi: "https://www.fastmock.site/mock/ea387bf6b0a1f5a493bc2af328a2d028",
+  },
+};
 export default {
-    env,
-    spacename:'ZHICHUANG',
-    mock:false,
-    ... EnvConfig[env]
-}
+  env,
+  spacename: "ZHICHUANG",
+  mock: false,
+  ...EnvConfig[env],
+};
