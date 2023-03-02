6d193582c562a56b4360e8b7b147d8cd3b39a701
duanli_666 <316149574@qq.com>
2023-03-02 09:24:16 +0800

    changeurl

--- src/conifg/index.js
+++ src/conifg/index.js
@@ -1,15 +1,15 @@
 const env = import.meta.env.MODE || "prod";
 const EnvConfig = {
   dev: {
-    baseApi: "119.3.202.71:3000",
+    baseApi: "http://119.3.202.71:3000",
     mockApi: "https://www.fastmock.site/mock/ea387bf6b0a1f5a493bc2af328a2d028",
   },
   test: {
-    baseApi: "119.3.202.71:3000",
+    baseApi: "http://119.3.202.71:3000",
     mockApi: "https://www.fastmock.site/mock/ea387bf6b0a1f5a493bc2af328a2d028",
   },
   prod: {
-    baseApi: "119.3.202.71:3000",
+    baseApi: "http://119.3.202.71:3000",
     mockApi: "https://www.fastmock.site/mock/ea387bf6b0a1f5a493bc2af328a2d028",
   },
 };
